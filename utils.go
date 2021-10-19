f82172b71e6c33a5063adca3d3ba00e90789d08c
learnforpractice <learnforpractice@gmail.com>
2021-10-19 16:44:31 +0800

    Add some helpful functions

--- utils.go
+++ utils.go
@@ -3,3 +3,12 @@ package chain
 import "unsafe"
 
 type PTR unsafe.Pointer
+
+func GetStringPtr(str string) unsafe.Pointer {
+	_str := (*stringHeader)(unsafe.Pointer(&str))
+	return _str.data
+}
+
+func GetBytesPtr(bs []byte) unsafe.Pointer {
+	return unsafe.Pointer(&bs[0])
+}
