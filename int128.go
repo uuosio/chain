d4e45e6e7dee1f17ca195daa36e870c8cb2f8e86
learnforpractice <learnforpractice@gmail.com>
2022-06-02 21:44:09 +0800

    Fix Int128.SetInt64 & Int128.Int64

--- int128.go
+++ int128.go
@@ -8,14 +8,30 @@ typedef struct {
 	int64_t hi;
 } int128;
 
+//*a = int128(*a)
 void int128_from_int64(int128* a, int64_t* b);
+
+//*b = int64(*a)
 void int128_to_int64(int128* a, int64_t* b);
 
+//*c = *a + *b
 void int128_add(int128* a, int128* b, int128* c);
+
+//*c = *a - *b
 void int128_sub(int128* a, int128* b, int128* c);
+
+//*b = abs(*a)
 void int128_abs(int128* a, int128* b);
+
+//*c = *a * *b
 void int128_mul(int128* a, int128* b, int128* c);
+
+//*c = *a / *b
 void int128_div(int128* a, int128* b, int128* c);
+
+//return 1 if *a > *b
+//return 0 if *a < *b
+//return -1 if *a == *b
 int int128_cmp(int128* a, int128* b);
 */
 import "C"
@@ -35,7 +51,7 @@ func NewInt128(lo uint64, hi uint64) Int128 {
 
 func NewInt128FromInt64(n int64) Int128 {
 	a := Int128{}
-	C.int128_from_int64((*C.int128)(unsafe.Pointer(&a)), &n)
+	C.int128_from_int64((*C.int128)(unsafe.Pointer(&a)), (*C.int64_t)(&n))
 	return a
 }
 
@@ -61,16 +77,20 @@ func (t *Int128) Size() int {
 }
 
 func (n *Int128) SetInt64(v int64) {
-	tmp := Int128{}
-	copy(n[:], tmp[:]) //memset
-	binary.LittleEndian.PutUint64(n[:], uint64(v))
+	C.int128_from_int64((*C.int128)(unsafe.Pointer(n)), (*C.int64_t)(&v))
 }
 
 func (n *Int128) Int64() int64 {
-	return int64(binary.LittleEndian.Uint64(n[:]))
+	ret := int64(0)
+	C.int128_to_int64((*C.int128)(unsafe.Pointer(n)), (*C.int64_t)(&ret))
+	return ret
 }
 
-func (t *Int128) Add(a, b *Int128) *Int128 {
+func (t *Int128) Add(b *Int128) *Int128 {
+	return t.AddEx(t, b)
+}
+
+func (t *Int128) AddEx(a, b *Int128) *Int128 {
 	_a := unsafe.Pointer(a)
 	_b := unsafe.Pointer(b)
 	_c := unsafe.Pointer(t)
@@ -79,6 +99,10 @@ func (t *Int128) Add(a, b *Int128) *Int128 {
 }
 
 func (t *Int128) Sub(a, b *Int128) *Int128 {
+	return t.SubEx(t, b)
+}
+
+func (t *Int128) SubEx(a, b *Int128) *Int128 {
 	_a := unsafe.Pointer(a)
 	_b := unsafe.Pointer(b)
 	_c := unsafe.Pointer(t)
@@ -93,14 +117,22 @@ func (t *Int128) Set(x *Int128) *Int128 {
 	return t
 }
 
-// func (t *Int128) Abs(a *Int128) *Int128 {
-// 	t.Set(a)
-// 	_a := unsafe.Pointer(t)
-// 	C.int128_abs((*C.int128)(_a), (*C.int128)(_a))
-// 	return t
-// }
+func (t *Int128) Abs() *Int128 {
+	return t.AbsEx(t)
+}
 
-func (t *Int128) Mul(a, b *Int128) *Int128 {
+func (t *Int128) AbsEx(a *Int128) *Int128 {
+	t.Set(a)
+	_a := unsafe.Pointer(t)
+	C.int128_abs((*C.int128)(_a), (*C.int128)(_a))
+	return t
+}
+
+func (t *Int128) Mul(b *Int128) *Int128 {
+	return t.MulEx(t, b)
+}
+
+func (t *Int128) MulEx(a, b *Int128) *Int128 {
 	_a := unsafe.Pointer(a)
 	_b := unsafe.Pointer(b)
 	_c := unsafe.Pointer(t)
@@ -108,7 +140,11 @@ func (t *Int128) Mul(a, b *Int128) *Int128 {
 	return t
 }
 
-func (t *Int128) Div(a, b *Int128) *Int128 {
+func (t *Int128) Div(b *Int128) *Int128 {
+	return t.DivEx(t, b)
+}
+
+func (t *Int128) DivEx(a, b *Int128) *Int128 {
 	_a := unsafe.Pointer(a)
 	_b := unsafe.Pointer(b)
 	_c := unsafe.Pointer(t)
